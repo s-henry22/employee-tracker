355aff704dc3f3bedf9d2873008f53c00ad2ffe3
Spencer Henry <spencerhenry94@gmail.com>
2021-05-20 19:53:00 -0400

    called array of sql data

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,23 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to sql database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username
+        user: 'root',
+        // Your MySQL password
+        password: 'Toronto!22',
+        database: 'company'
+    },
+    console.log('Connected to the company database.')
+);
+
+db.query(`SELECT * FROM departments`, (err, rows) => {
+    console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
