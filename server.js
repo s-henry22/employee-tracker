8cc1b83f01a9b027754384145c3518a60cdff6d4
Spencer Henry <spencerhenry94@gmail.com>
2021-05-20 20:09:59 -0400

    added query statements

--- server.js
+++ server.js
@@ -21,9 +21,38 @@ const db = mysql.createConnection(
     console.log('Connected to the company database.')
 );
 
-db.query(`SELECT * FROM departments`, (err, rows) => {
+/*db.query(`SELECT * FROM departments`, (err, rows) => {
     console.log(rows);
+});*/
+
+// GET a single department
+/*db.query(`SELECT * FROM departments WHERE id = 1`, (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+});*/
+
+// Delete a department
+/*db.query(`DELETE FROM departments WHERE id = ?`, 1, (err, result) => {
+    if(err) {
+        console.log(err);
+    }
+    console.log(result);
+});*/
+
+// Create a department
+const sql = `INSERT INTO departments (id, department_name)
+            VALUES (?,?)`;
+const params = [1, 'Sales'];
+
+db.query(sql, params, (err, result) => {
+    if(err) {
+        console.log(err);
+    }
+    console.log(result);
 });
+
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
